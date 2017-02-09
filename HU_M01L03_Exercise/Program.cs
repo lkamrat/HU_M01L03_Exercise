f2c7581059326c68eeb21948652ba40620868a0c
lkamrat <kamrat9@gmail.com>
2017-02-09 18:24:57 +0200

    Q1

--- HU_M01L03_Exercise/Program.cs
+++ HU_M01L03_Exercise/Program.cs
@@ -5,20 +5,22 @@
     {
         static void Main(string[] args)
         {
-            // Define double numbers (we need the avarage of those numbers) from user input
-            Console.WriteLine("Enter number 01");
-            double numOne = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter number 02");
-            double numTwo = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter number 03");
-            double NumThree = double.Parse(Console.ReadLine());
+            // Define double numbers (we need the avarage & multiplying of those numbers) from user input
+            System.Console.WriteLine("Enter number 01");
+            double numOne = double.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Enter number 02");
+            double numTwo = double.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Enter number 03");
+            double NumThree = double.Parse(System.Console.ReadLine());
 
             // Print the numbers
-            Console.WriteLine("One: {0}, Two {1}, Three {2}", numOne, numTwo, NumThree);
+            System.Console.WriteLine("One: {0}, Two {1}, Three {2}", numOne, numTwo, NumThree);
 
             // Print the numbers avarage
-            Console.WriteLine("The avarage is: {0}", (numOne + numTwo + NumThree) / 3);
+            System.Console.WriteLine("The avarage is: {0}", (numOne + numTwo + NumThree) / 3);
 
+            // Print the numbers multiplying
+            System.Console.WriteLine("The multiplying is: {0}", numOne * numTwo * NumThree);
 
         }
     }
