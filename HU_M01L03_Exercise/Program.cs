5a2478c0efa0aa7af19ec8507ee60abaa0c6bf09
lkamrat <kamrat9@gmail.com>
2017-02-09 18:14:20 +0200

    Initial commit

--- HU_M01L03_Exercise/Program.cs
+++ HU_M01L03_Exercise/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿ namespace HU_M01L03_Exercise
 
-namespace HU_M01L03_Exercise
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Define double numbers (we need the avarage of those numbers) from user input
+            Console.WriteLine("Enter number 01");
+            double numOne = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number 02");
+            double numTwo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number 03");
+            double NumThree = double.Parse(Console.ReadLine());
+
+            // Print the numbers
+            Console.WriteLine("One: {0}, Two {1}, Three {2}", numOne, numTwo, NumThree);
+
+            // Print the numbers avarage
+            Console.WriteLine("The avarage is: {0}", (numOne + numTwo + NumThree) / 3);
+
+
         }
     }
 }
